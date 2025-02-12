8524f062d5a25b197bb81925152b43db56047ab1
zfny <zilfanynur@gmail.com>
2025-02-12 22:27:48 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -8,3 +8,22 @@ menuIcon.addEventListener('click', () => {
         document.querySelector(".nav-links").classList.toggle("active");
     }
 })
+// Inisialisasi EmailJS
+(function () {
+    emailjs.init("L9BAqPWSHhojCZrPl"); // Ganti dengan User ID dari EmailJS
+})();
+
+document.getElementById("contact-form").addEventListener("submit", function (event) {
+    event.preventDefault(); // Mencegah reload halaman
+
+    emailjs.sendForm('service_ht9cggk', 'template_a3p1gur', this, 'L9BAqPWSHhojCZrPl')
+        .then(function (response) {
+            console.log("SUCCESS!", response);
+            alert("Message sent successfully!");
+        }, function (error) {
+            console.log("FAILED...", error);
+            alert("Failed to send message. Please try again.");
+        });
+});
+
+
