268b7cefced3bbeb7b41c79579258f11f6714c69
zfny <zilfanynur@gmail.com>
2025-02-13 00:21:49 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const menuIcon = document.querySelector(".menu-icon")
+const menuIcon= document.querySelector(".menu-icon")
 const navList = document.querySelector("ul");
 
 menuIcon.addEventListener('click', () => {
@@ -8,35 +8,19 @@ menuIcon.addEventListener('click', () => {
         document.querySelector(".nav-links").classList.toggle("active");
     }
 })
+// Inisialisasi EmailJS dengan user ID Anda
+emailjs.init('L9BAqPWSHhojCZrPl'); // Ganti dengan user ID Anda
 
-    (function () {
-        emailjs.init("L9BAqPWSHhojCZrPl"); // Ganti dengan User ID dari EmailJS
-    })();
+document.getElementById("contact-form").addEventListener("submit", function(event){
+    event.preventDefault(); // Menghentikan form dari reload halaman
 
-document.getElementById("contact-form").addEventListener("submit", function (event) {
-    event.preventDefault(); // Mencegah reload halaman
-
+    // Kirim email melalui EmailJS
     emailjs.sendForm('service_ht9cggk', 'template_a3p1gur', this, 'L9BAqPWSHhojCZrPl')
-        .then(function (response) {
-            console.log("SUCCESS!", response);
+        .then(function(response) {
+            console.log('Success!', response);
             alert("Message sent successfully!");
-        }, function (error) {
-            console.log("FAILED...", error);
+        }, function(error) {
+            console.log('Failed...', error);
             alert("Failed to send message. Please try again.");
         });
 });
-
-window.addEventListener("scroll", function () {
-
-    const content = document.querySelector(".content");
-
-
-    if (window.scrollY > 50) {
-        content.classList.add("blur-effect");
-    } else {
-        content.classList.remove("blur-effect");
-    }
-});
-
-
-
